4c4e4c8cab2dbe0525b4a204d000577930033f90
Joe McCarthy <179146301+joe-mccarthy@users.noreply.github.com>
2024-08-31 15:41:09 +0100

    changed the assert to math.isclose to please sonar #6

--- tests/utilities/test_conversions.py
+++ tests/utilities/test_conversions.py
@@ -1,19 +1,22 @@
 from src.app.utilities import conversions
+import math
 
 
 def test_microseconds_to_hundredth_second():
     microseconds = 10000
     seconds = conversions.microsecond_to_seconds(microseconds)
-    assert seconds == 0.01
+    assert math.isclose(seconds,0.01)
+
 
 
 def test_microseconds_to_tenth_second():
     microseconds = 100000
     seconds = conversions.microsecond_to_seconds(microseconds)
-    assert seconds == 0.1
+    assert math.isclose(seconds,0.1)
+
 
 
 def test_simple_micro_to_seconds():
     microseconds = 1000000
     seconds = conversions.microsecond_to_seconds(microseconds)
-    assert seconds == 1.0
+    assert math.isclose(seconds,1.0)
