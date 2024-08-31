f7fa9fa6319f657d484980da2c9a69697911900c
Joe McCarthy <179146301+joe-mccarthy@users.noreply.github.com>
2024-08-31 14:38:03 +0100

    #5 removing the TODOs for image timeout have raised an issue to resolve the TODOs #10

--- src/app/capture/imaging.py
+++ src/app/capture/imaging.py
@@ -40,7 +40,7 @@ def __capture_image(observation: Observation, capture: Capture) -> Capture:
     call = f"{command} -o {filename} {exposure_settings} {white_balance} {switches}"
 
     try:
-        image_timeout = 70  # TODO this needs to be from the configuration
+        image_timeout = 70 
         log.debug("image capture timeout: %s", image_timeout)
         subprocess.run(
             call,
@@ -48,8 +48,8 @@ def __capture_image(observation: Observation, capture: Capture) -> Capture:
             check=True,
             stdout=subprocess.DEVNULL,
             stderr=subprocess.STDOUT,
-            timeout=image_timeout,
-        )  # TODO this needs to be from the configuration
+            timeout=image_timeout
+        )
         log.info("image capture completed")
         calculate_next_exposure_value(filename, capture)
     except Exception as e:
