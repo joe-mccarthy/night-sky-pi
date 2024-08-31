0fb3d6f69bcfa3b5c1c8977de61a9682dd0f6b56
Joe McCarthy <179146301+joe-mccarthy@users.noreply.github.com>
2024-08-31 15:41:24 +0100

    run black formatting #6

--- tests/utilities/test_conversions.py
+++ tests/utilities/test_conversions.py
@@ -5,18 +5,16 @@ import math
 def test_microseconds_to_hundredth_second():
     microseconds = 10000
     seconds = conversions.microsecond_to_seconds(microseconds)
-    assert math.isclose(seconds,0.01)
-
+    assert math.isclose(seconds, 0.01)
 
 
 def test_microseconds_to_tenth_second():
     microseconds = 100000
     seconds = conversions.microsecond_to_seconds(microseconds)
-    assert math.isclose(seconds,0.1)
-
+    assert math.isclose(seconds, 0.1)
 
 
 def test_simple_micro_to_seconds():
     microseconds = 1000000
     seconds = conversions.microsecond_to_seconds(microseconds)
-    assert math.isclose(seconds,1.0)
+    assert math.isclose(seconds, 1.0)
